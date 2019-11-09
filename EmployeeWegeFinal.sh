85b87c5c8a03201583af22b0ad26840999ad86ae
Laxman Bhosale <43477240+luckyNB@users.noreply.github.com>
2019-11-09 18:19:43 +0530

    Refactoring present to empPresent

--- EmployeeWegeFinal.sh
+++ EmployeeWegeFinal.sh
@@ -2,9 +2,9 @@
 
 hours=8;
 dailyWage=300;
-present=$((RANDOM%10))
+empPresent=$((RANDOM%10))
 
-if [ $present -gt 0 ]
+if [ $empPresent -gt 0 ]
 then
 wage=$(($hours * $dailyWage))
 echo "Employee Present"
