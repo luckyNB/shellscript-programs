804f03da5f2faf128cd9aabfdf66ec9d05be8445
Laxman Bhosale <43477240+luckyNB@users.noreply.github.com>
2019-11-09 17:17:51 +0530

    Daily Wages added

--- EmployeeWegeFinal.sh
+++ EmployeeWegeFinal.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
+
+hours=8;
+dailyWage=300;
 present=$((RANDOM%10))
 
 if [ $present -gt 0 ]
 then
-
+wage=$(($hours * $dailyWage))
 echo "Employee Present"
 else
  echo "Employee not present"
