5cd32ab176e01618baa365f40c6b5876827f6187
luckyNB <20150773@dbatu.ac.in>
2019-11-11 17:14:45 +0530

    Dictionary conflicts are resolved and made some changes

--- ArithmeticOps.sh
+++ ArithmeticOps.sh
@@ -7,14 +7,15 @@ echo "Enter the value of a,b and c"
 	read b 
 	read c
 
-echo $a $b $c
+
 result=$(($a +$b *$c))
-Computation[counter++]=$result
-echo $result
+Computation[$((counter++))]=$result
+
 result=$(($a +$b / $c))
-echo $result
-Computation[counter++]=$result
+Computation[$((counter++))]=$result
+
 result=$(($a % $b + $c))
-echo $result
-Computation[counter++]=$result
+Computation[$((counter++))]=$result
+
 echo  "All results of Computation" ${Computation[@]}
+echo "Number of data" ${#Computation[@]}
