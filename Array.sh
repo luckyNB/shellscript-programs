f176df7b6d814ef86beb63a766cde645f77bc7d0
Laxman Bhosale <43477240+luckyNB@users.noreply.github.com>
2019-11-11 13:08:35 +0530

    CustordApple and PineApple is added

--- Array.sh
+++ Array.sh
@@ -3,5 +3,6 @@ counter=0
 fruits[((counter++))]="Apple"
 fruits[((counter++))]="Banana"
 fruits[((counter++))]="Orange"
-
+fruits[((counter++))]="PineApple"
+fruits[((counter++))]="CustordApple"
 echo ${fruits[@]}
