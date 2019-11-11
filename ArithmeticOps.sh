8b875da75eb28e4550f2ecbdaa200c4c0e4a8c87
luckyNB <20150773@dbatu.ac.in>
2019-11-11 16:57:46 +0530

    Dictionary features are added to the program to store all results

--- ArithmeticOps.sh
+++ ArithmeticOps.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
+counter=0
+declare -A Computation
 
 echo "Enter the value of a,b and c"
 	read a 
@@ -7,9 +9,12 @@ echo "Enter the value of a,b and c"
 
 echo $a $b $c
 result=$(($a +$b *$c))
+Computation[counter++]=$result
 echo $result
 result=$(($a +$b / $c))
 echo $result
-
+Computation[counter++]=$result
 result=$(($a % $b + $c))
 echo $result
+Computation[counter++]=$result
+echo  "All results of Computation" ${Computation[@]}
