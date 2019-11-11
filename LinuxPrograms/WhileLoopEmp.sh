f2c863dbdee1b457c226efc95fadc40613ed2a75
luckyNB <20150773@dbatu.ac.in>
2019-11-11 12:54:31 +0530

    array program of salary array is done with function

--- LinuxPrograms/WhileLoopEmp.sh
+++ LinuxPrograms/WhileLoopEmp.sh
@@ -3,6 +3,7 @@ counter=1
 fullTime=1
 partTime=2
 hours=50
+c=0
   while [ $hours -gt $counter ]
 
 	do
@@ -18,6 +19,7 @@ isPresent=$((RANDOM % 3))
 	fi
 
 	salary=$(($counter * 100))
+totalSalary[((c++))]=$salary
 done  
 
 
