80e74615cce96972fa5fa3552f9c46b098238495
luckyNB <20150773@dbatu.ac.in>
2019-11-11 16:18:26 +0530

    After computing (a+b*c) another computation is done of equation (c+a/b)

--- ArithmeticOps.sh
+++ ArithmeticOps.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
 
 echo "Enter the value of a,b and c"
-read a 
-read b 
-read c
+	read a 
+	read b 
+	read c
+
 echo $a $b $c
 result=$(($a +$b *$c))
+echo $result
+result=$(($a +$b / $c))
 echo $result
