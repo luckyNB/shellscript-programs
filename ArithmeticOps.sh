10bff0f4234895cae8788d29926e7ded347901d1
luckyNB <20150773@dbatu.ac.in>
2019-11-12 09:17:27 +0530

    Optimization of code is done and formatted with proper Identation

--- ArithmeticOps.sh
+++ ArithmeticOps.sh
@@ -1,37 +1,24 @@
 #!/bin/bash -x
-counter=0
-temp=0
-declare -A Computation
-
-echo "Enter the value of a,b and c"
+	counter=0
+	temp=0
+	declare -A Computation
 	read a 
 	read b 
 	read c
+	result=$(($a +$b *$c))
+	Computation[$((counter++))]=$result
+	result=$(($a +$b / $c))
+	Computation[$((counter++))]=$result
+	result=$(($a % $b + $c))
+	Computation[$((counter++))]=$result
+	echo  "All results of Computation" ${Computation[@]}
+	echo "Number of data" ${#Computation[@]}
 
-
-result=$(($a +$b *$c))
-Computation[$((counter++))]=$result
-
-result=$(($a +$b / $c))
-Computation[$((counter++))]=$result
-
-result=$(($a % $b + $c))
-Computation[$((counter++))]=$result
-
-echo  "All results of Computation" ${Computation[@]}
-echo "Number of data" ${#Computation[@]}
-
-for key in ${!Computation[@]}
-do
-ComputationArray[((counter++))]=${Computation[$key]}
-done
-echo    "Computation Array is::" ${ComputationArray[@]}
-
-echo "After Sorting in Ascending Order" 
-
-echo $(printf "%s\n" ${ComputationArray[@]} | sort -n)
-  
-echo "After Sorting in descending Order" 
-
-echo $(printf "%s\n" ${ComputationArray[@]} | sort -nr)
+	for key in ${!Computation[@]}
+		do
+		ComputationArray[((counter++))]=${Computation[$key]}
+	done
+	echo    "Computation Array is::" ${ComputationArray[@]}
+	echo "Ascendig Order" $(printf "%s\n" ${ComputationArray[@]} | sort -n)
+  	echo "Descending Order" $(printf "%s\n" ${ComputationArray[@]} | sort -nr)
   
